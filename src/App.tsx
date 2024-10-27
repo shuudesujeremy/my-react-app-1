6c5979475a72478872165eba98e59c8449ed1107
shuudesujeremy <iamjeremymendez234@gmail.com>
2024-10-27 20:48:29 +0800

    added interface for function square

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
-
-function Square({value, onSquareClick}) {
+interface SquareProps {
+  value: string;
+  onSquareClick: () => void;
+}
+function Square({value, onSquareClick}: SquareProps) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
